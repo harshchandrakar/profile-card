410eddfbbc77c40a51232472e01aeeee5ee2131e
Harsh Kumar Chandrakar <72716403+harshchandrakar@users.noreply.github.com>
2022-01-06 14:40:15 +0530

    made changes

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -1,7 +1,8 @@
 import React from "react";
 import classes from "./card.module.css";
 function Card({ children }) {
-  return <div className={classes.card}>{children}</div>;
+  return <div className={classes.card}>{children}
+  <h1>HIIIII </h1></div>;
 }
 
 export default Card;
